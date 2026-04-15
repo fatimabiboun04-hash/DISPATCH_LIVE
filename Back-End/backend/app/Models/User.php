5aeb9f0597e5ed64ea41cccfbd158a5e73d0a7c1
fatima <fatimabiboun04@gmail.com>
2026-04-15 22:08:40 +0100

    add backend files

--- Back-End/backend/app/Models/User.php
+++ Back-End/backend/app/Models/User.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -12,34 +11,41 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
-        'name',
+        'nom',
         'email',
         'password',
+        'role',
+        'equipe_id',
+        'rating',
+        'description',
+        'is_active',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
     protected $casts = [
         'email_verified_at' => 'datetime',
-        'password' => 'hashed',
+        'password'          => 'hashed',
+        'is_active'         => 'boolean',
+        'rating'            => 'integer',
     ];
+
+    public function equipe()
+    {
+        return $this->belongsTo(Equipe::class);
+    }
+
+    public function plannings()
+    {
+        return $this->hasMany(Planning::class);
+    }
+
+    public function repos()
+    {
+        return $this->hasMany(Repo::class);
+    }
 }
--- Back-End/backend/routes/api.php
+++ Back-End/backend/routes/api.php
@@ -1,19 +1,52 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\EquipeController;
+use App\Http\Controllers\TacheController;
+use App\Http\Controllers\PlanningController;
+use App\Http\Controllers\ReposController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+// Public
+Route::post('/login', [AuthController::class, 'login']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function () {
+
+    Route::post('/logout',      [AuthController::class, 'logout']);
+    Route::get('/me',           [AuthController::class, 'me']);
+
+    // Employe + Admin
+    Route::get('/mon-profil',   [UserController::class,   'monProfil']);
+    Route::get('/mon-planning', [PlanningController::class,'monPlanning']);
+    Route::get('/equipes',      [EquipeController::class,  'index']);
+    Route::get('/taches',       [TacheController::class,   'index']);
+
+    // Employe
+    Route::post('/repos',       [ReposController::class, 'store']);
+    Route::get('/mes-demandes', [ReposController::class, 'mesDemandes']);
+
+    // Admin only
+    Route::middleware('admin')->group(function () {
+
+        Route::apiResource('users',    UserController::class);
+        Route::apiResource('equipes',  EquipeController::class)->except(['index']);
+        Route::apiResource('taches',   TacheController::class)->except(['index']);
+        Route::apiResource('plannings', PlanningController::class);
+
+        Route::post('plannings/{id}/taches',
+            [PlanningController::class, 'addTache']);
+        Route::delete('plannings/{id}/taches/{tache_id}',
+            [PlanningController::class, 'removeTache']);
+
+        Route::get('/repos',                   [ReposController::class, 'index']);
+        Route::put('/repos/{id}/valider',      [ReposController::class, 'valider']);
+        Route::put('/repos/{id}/refuser',      [ReposController::class, 'refuser']);
+        Route::get('/notifications',           [ReposController::class, 'notifications']);
+        Route::put('/notifications/{id}/read', [ReposController::class, 'markAsRead']);
+
+        Route::get('/dashboard',              [DashboardController::class, 'index']);
+        Route::get('/rapport-hebdomadaire',   [DashboardController::class, 'rapportHebdomadaire']);
+    });
 });
